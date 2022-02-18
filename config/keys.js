dc678ae39dc6c8257acaeea00e7432298988c9be
Himanshu Ramchandani <37770869+hemansnation@users.noreply.github.com>
2022-02-18 11:18:28 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://windowUser:user12345@cluster0.ljuth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://user:password@cluster0.ljuth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
